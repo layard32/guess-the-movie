9e444d8d8a4a3e420c342fbab7184ecb9f674ca4
layard <114928369+layard32@users.noreply.github.com>
2025-03-29 15:55:00 +0100

    Update resetPassword.tsx

--- src/pages/resetPassword.tsx
+++ src/pages/resetPassword.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import DefaultLayout from "@/layouts/default";
 import ResetPasswordForm from "@/components/authComponents/resetPasswordForm";
+import { checkUserRedirect } from "@/hooks/checkUserRedirect";
 
 const resetPassword = () => {
+  // per sicurezza controllo se c'è un utente: se non c'è lo rimando alla home
+  const user = checkUserRedirect("/");
+
   return (
     <DefaultLayout>
       <div className="w-full flex flex-col items-center justify-center">
